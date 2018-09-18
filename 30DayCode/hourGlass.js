434e072345c8131fc30f242f6bfcbf21c4eb67b2
Cuitlahuac Rodrigo Aristeo Campos <cuitlagua@gmail.com>
2018-09-17 22:12:08 -0500

    Create hourGlass.js

---
+++ 30DayCode/hourGlass.js
@@ -0,0 +1,39 @@
+let arr = 
+[[0, -4, -6, 0, -7, -6],
+[-1, -2, -6, -8, -3, -1],
+[-8, -4, -2, -8, -8, -6],
+[-3, -1, -2, -5, -7, -4],
+[-3, -5, -3, -6, -6, -6],
+[-3, -6, 0, -8, -6, -7]]
+
+//expected -19
+
+function hourGlasses(arr) {
+    let maxSuma = 0;
+    for (let row = 0; row < 4; row++) {
+        let sum = 0;
+        for (let col = 0; col < 4; col++) {
+              sum = 
+              arr[row][col] +
+              arr[row][col + 1] + 
+              arr[row][col + 2] + 
+              arr[row + 1][col + 1] +
+              arr[row + 2][col] +
+              arr[row + 2][col + 1] + 
+              arr[row + 2][col + 2]
+            if (sum < 0 && sum <= maxSuma || sum > 0 && sum >= maxSuma) {
+                maxSuma = sum;
+            }
+            sum = 0;
+        }
+    }
+    return maxSuma
+}
+console.log(hourGlasses(arr))
+
+[ 0[ 1, 1, 1, 0, 0, 0 ],
+  1[ 0, 1, 0, 0, 0, 0 ],
+  2[ 1, 1, 1, 0, 0, 0 ],
+  3[ 0, 0, 2, 4, 4, 0 ],
+  4[ 0, 0, 0, 2, 0, 0 ],
+  6[ 0, 0, 1, 2, 4, 0 ] ]
